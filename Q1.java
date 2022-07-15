e63491934d960a12e9a1144ddfd9805104ad323d
Kailash Sahu <85280661+Kailashsk@users.noreply.github.com>
2022-07-16 04:42:17 +0530

    Create Q1.java

---
+++ Q1.java
@@ -0,0 +1,14 @@
+2.	import java.util.Scanner;
+3.	
+4.	public class diss1{
+5.	
+6.	    public static void main(String[] args) {
+7.	        Scanner sc = new Scanner(System.in);
+8.	        System.out.println("Input height of the person:"); 
+9.	        double h = sc.nextDouble();
+10.	         if (h >= 6.0){
+11.	             System.out.println("The person is tall.");
+12.	         }
+13.	    } 
+14.	
+15.	}
